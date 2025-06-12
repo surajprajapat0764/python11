82fd5df80622d035da491883b5d33d3301a9b837
surajprajapat0764 <surajprajapat0764@gmail.com>
2025-06-12 09:59:46 +0530

    new

---
+++ data science python/Basic python/str.py
@@ -0,0 +1,17 @@
+##string is inntruble -> connot be changed
+## denoted by - ""
+name = "upflair pvt"
+#print(name.swapcase())
+#print(name.strip())
+# print(name.lstrip())
+# print(name.rstrip())
+# print(name.replace("pvt","ltd"))
+# print(name.split())
+#print(name.partition("d"))
+#print(name.isalnum())
+# age = 17 
+# #text = "my age is" + age
+# text = f"my age is {age} "
+# print(text)
+#text = "my name is \'jai\'"
+#print(text)
