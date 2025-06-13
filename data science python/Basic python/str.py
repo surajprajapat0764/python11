4078120923954f6569ca3aa98eff91e816f8d1fc
surajprajapat0764 <surajprajapat0764@gmail.com>
2025-06-13 11:55:08 +0530

    python

--- data science python/Basic python/str.py
+++ data science python/Basic python/str.py
@@ -3,15 +3,15 @@
 name = "upflair pvt"
 #print(name.swapcase())
 #print(name.strip())
-# print(name.lstrip())
+#print(name.lstrip())
 # print(name.rstrip())
-# print(name.replace("pvt","ltd"))
+#print(name.replace("pvt","ltd"))
 # print(name.split())
 #print(name.partition("d"))
 #print(name.isalnum())
-# age = 17 
-# #text = "my age is" + age
-# text = f"my age is {age} "
-# print(text)
+#age = 17 
+#text = "my age is" + age
+#text = f"my age is {age} "
+#print(text)
 #text = "my name is \'jai\'"
 #print(text)
